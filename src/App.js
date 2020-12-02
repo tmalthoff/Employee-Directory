52c0e35afb86794e926d43db63f05d23283b947f
Thomas Althoff <matthewalthoff1@gmail.com>
2020-12-02 11:34:10 -0600

    Basic table with data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
-}
+export default class App extends React.Component {
+  state = { results: [] }
 
-export default App;
+  componentDidMount() {
+    fetch('https://randomuser.me/api/?results=25')
+      .then(response => response.json())
+      .then(data => this.setState({ results: data.results }))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <table>
+          <thead>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Cellphone</th>
+          </thead>
+          <tbody>
+            { this.state.results.map(user => (
+              <tr>
+                <td>{user.name.first} {user.name.last}</td>
+                <td>{user.email}</td>
+                <td>{user.phone}</td>
+                <td>{user.cell}</td>
+              </tr>
+            )) }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
